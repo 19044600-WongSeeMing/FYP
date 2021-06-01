b8b432b965077039e03be50c76e0cebf8c39c989
See Ming <See_ming_wong@outlook.com>
2021-06-01 13:35:01 +0800

    //signup function (not complete)

--- FYP01/Controllers/AccountController.cs
+++ FYP01/Controllers/AccountController.cs
@@ -128,7 +128,7 @@ namespace FYP01.Controllers
             {
                 ViewData["Message"] = "Invalid Input";
                 ViewData["MsgType"] = "warning";
-                return View("UserRegister");
+                return View("SignUp");
             }
             else
             {
@@ -136,37 +136,17 @@ namespace FYP01.Controllers
                 string insert =
                    @"INSERT INTO MesahUser(UserId, UserPw, FullName, Email, Address, PostalCode, Phone, UserRole) 
                         VALUES('{0}',HASHBYTES('SHA1','{1}'),'{2}','{3}','{4}','{5}','{6}','member')";
-                if (DBUtl.ExecSQL(insert, usr.UserId, usr.UserPw, usr.FullName, usr.Email,usr.Address,usr.PostalCode,usr.Phone) == 1)
+                if (DBUtl.ExecSQL(insert, usr.UserId, usr.UserPw, usr.FullName, usr.Email,usr.Address,usr.PostalCode,usr.Phone,usr.UserRole) == 1)
                 {
-                    string template = @"Hi {0},<br/><br/>
-                               Welcome to Mesah Delicacies!
-                               Your userid is <b>{1}</b> and password is <b>{2}</b>.
-                               <br/><br/>Manager";
-             //       string title = "Registration Successul - Welcome";
-                    string message = String.Format(template, usr.FullName, usr.UserId, usr.UserPw);
-                  //  string result = "";
-                    
-            //        bool outcome = false;
-                    // TODO: L10 Task 2b - Call EmailUtl.SendEmail to send email
-                    //                     Uncomment the following line with you are done
-           //         outcome = EmailUtl.SendEmail(usr.Email, title, message, out result);
-          /*          if (outcome)
-                    {
-                        ViewData["Message"] = "User Successfully Registered";
-                        ViewData["MsgType"] = "success";
-                    }
-                    else
-                    {
-                        ViewData["Message"] = result;
-                        ViewData["MsgType"] = "warning";
-                    }*/
+                    ViewData["Message"] = "User Created";
+                    ViewData["MsgType"] = "Success";
                 }
                 else
                 {
                     ViewData["Message"] = DBUtl.DB_Message;
                     ViewData["MsgType"] = "danger";
                 }
-                return View("UserRegister");
+                return RedirectToAction("Login");
             }
         }
 
