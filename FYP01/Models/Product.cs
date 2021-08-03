e4a2275d6dd4fbbdc0cf5e9e7df2310193ce20ee
See Ming <See_ming_wong@outlook.com>
2021-08-03 12:16:49 +0800

    //

--- FYP01/Models/Product.cs
+++ FYP01/Models/Product.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using FYP01.Models;
 using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace FYP01.Models
@@ -9,7 +10,11 @@ namespace FYP01.Models
     public partial class Product
     {
         public int ProductId { get; set; }
+        
+        [Required(ErrorMessage = "Please enter product name!")]
         public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "Please enter price!")]
         public double Price { get; set; }
         public int CategoryId { get; set; }
 
--- FYP01/Models/Testimonial.cs
+++ FYP01/Models/Testimonial.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using FYP01.Models;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace FYP01.Models
@@ -8,7 +9,11 @@ namespace FYP01.Models
     public partial class Testimonial
     {
         public int TestID { get; set; }
+        
+        [Required(ErrorMessage = "Please enter customer name!")]
         public string TestName { get; set; }
+        
+        [Required(ErrorMessage = "Please enter testimonial!")]
         public string Testi { get; set; }
         public string ProductName { get; set; }
 
